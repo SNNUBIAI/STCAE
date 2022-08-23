0bb5b74ab763131be40dca9308af63977314c0dd
lyh <412929473@qq.com>
2022-08-23 16:46:07 +0800

    loadhcp change load

--- datasets/loadhcp.py
+++ datasets/loadhcp.py
@@ -31,6 +31,7 @@ class LoadHCP(data.Dataset):
 		self.img = self.img.unsqueeze(1)
 
 	def __getitem__(self, item):
+
 		return self.img[item]
 
 	def __len__(self):
@@ -44,22 +45,29 @@ class LoadHCPSub(data.Dataset):
 		self.mask_img = nib.load(mask_path).get_fdata()
 		self.filename = os.listdir(HCP_path)
 		self.task_file = [name for name in self.filename if task in name]
+		self.hcp_path = HCP_path
 
 		if load_num != None:
 			self.task_file = self.task_file[:load_num]
-		fmri_masked_list = []
-		for file in self.task_file:
-			fmri_masked = np.load(HCP_path + file)
-			x_train_3D = inverse_transform(fmri_masked, self.mask_img)
-			img = torch.tensor(x_train_3D, dtype=torch.float)
-			img = img.permute(3, 0, 1, 2)
-			img = img.unsqueeze(0)
-			fmri_masked_list.append(img)
-
-		self.img = torch.cat(fmri_masked_list, dim=0)
+		# fmri_masked_list = []
+		# for file in self.task_file:
+		# 	fmri_masked = np.load(HCP_path + file)
+		# 	x_train_3D = inverse_transform(fmri_masked, self.mask_img)
+		# 	img = torch.tensor(x_train_3D, dtype=torch.float)
+		# 	img = img.permute(3, 0, 1, 2)
+		# 	img = img.unsqueeze(0)
+		# 	fmri_masked_list.append(img)
+		#
+		# self.img = torch.cat(fmri_masked_list, dim=0)
 
 	def __getitem__(self, item):
-		return self.img[item]
+		fmri_masked = np.load(self.hcp_path + self.task_file[item])
+		x_train_3D = inverse_transform(fmri_masked, self.mask_img)
+		img = torch.tensor(x_train_3D, dtype=torch.float)
+		del x_train_3D
+		img = img.permute(3, 0, 1, 2)
+		img = img.unsqueeze(0)
+		return img
 
 	def __len__(self):
-		return self.img.shape[0]
+		return len(self.task_file)
